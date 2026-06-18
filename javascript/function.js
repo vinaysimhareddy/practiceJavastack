fcea2c29cca7553124884faa9502b2d427787435
vinaysimhareddy <pvinaysimhareddy333@gmail.com>
2026-06-18 17:47:13 +0530

    day 22

--- javascript/function.js
+++ javascript/function.js
@@ -15,18 +15,47 @@
 // console.log(typeof person);
 // console.log(typeof add);
 
-function AB(){
-    BC();
-}
-function BC(){
-    console.trace("trace:");
-}
+// function AB(){
+//     BC();
+// }
+// function BC(){
+//     console.trace("trace:");
+// }
 
-function add(a,b){
-    console.log("trace:");
-    AB();
-    return a+b;
-}
+// function add(a,b){
+//     console.log("trace:");
+//     AB();
+//     return a+b;
+// }
 
-console.log(add(1,3));
-console.log(add(5,5));
+// console.log(add(1,3));
+// console.log(add(5,5));
+
+//normal function or name function 
+// function add() {
+//     console.log("adding");
+// }
+// debugger
+
+// //function expression or anonymous function
+// var greet = function () {
+//     console.log("Hello anonymous function");
+// }
+
+// //arrow function
+// var greet1 = () => {
+//     console.log("Hello Arrow function")
+// }
+// greet();
+// greet1();
+// add();
+
+let person = {
+    name: "vinay",
+    city: "nandyal",
+    greeet: function () {
+        console.log("Hello " + this.name + " welcome to " + this.city);
+    }
+}
+person.greeet();
+person.greeet.call()
