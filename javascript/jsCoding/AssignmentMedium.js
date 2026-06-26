500366bd9b0f39919e641788f884301f62bdd11a
vinaysimhareddy <pvinaysimhareddy333@gmail.com>
2026-06-26 19:43:12 +0530

    js coding practice

---
+++ javascript/jsCoding/AssignmentMedium.js
@@ -0,0 +1,25 @@
+//currency converter
+// const usdToEur = 0.92;
+// const usdToGbp = 0.79;
+// const usdToInr = 83.50;
+// const usdToJpy = 149.50;
+
+// const amountInUSD = 10;
+
+// const eur = (amountInUSD * usdToEur).toFixed(2);
+// const gbp = (amountInUSD * usdToGbp).toFixed(2);
+// const inr = (amountInUSD * usdToInr).toFixed(2);
+// const jpy = (amountInUSD * usdToJpy).toFixed(2);
+
+// console.log(`
+// Currency Converter
+// ======================
+// USD $${amountInUSD.toFixed(2)} converts to:
+
+// 🇪🇺 EUR : €${eur}
+// 🇬🇧 GBP : £${gbp}
+// 🇮🇳 INR : ₹${inr}
+// 🇯🇵 JPY : ¥${jpy}
+// ======================
+// `);
+
